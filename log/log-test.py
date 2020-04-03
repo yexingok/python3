22d2ab945f807ed53b01ccdd76c1ff93ce3d6ba0
Yexing <yexingok@gmail.com>
2020-04-03 15:48:11 +0800

    Inital create general logger function with a steamhandler and a filehandler

--- log/log-test.py
+++ log/log-test.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python
 
 from logger import create_logger
+import os
 
-LOGGER = create_logger(level='DEBUG')
+#Usage: 
+logfile = os.path.basename(__file__).replace(".py","",1)
+LOGGER = create_logger(app_name=logfile, level='DEBUG')
 
 def yxtest1():
     LOGGER.info("Running %d", 1)
 
 def yxtest2():
-    LOGGER.error("Running %s", 2)
+    LOGGER.warning("Running %s", 2)
+
+def yxtest3():
+    LOGGER.error("Running %s", 3)
+
+def yxtest4():
+    LOGGER.critical("Running %s", 4)
 
 def main():
     LOGGER.debug("Running %03d", 0)
     yxtest1()
     yxtest2()
+    yxtest3()
+    yxtest4()
 
 if __name__ == "__main__":
    main()
+   
--- log/logger.py
+++ log/logger.py
@@ -1,16 +1,40 @@
 #!/usr/bin/env python
 
 import logging
+import os
 
-def create_logger(app_name=None, level='WARNING'):
-    logger = logging.getLogger(app_name or __name__)
-    logger.setLevel(level)
-    log_format = '[%(asctime)-15s] [%(levelname)08s] [%(filename)s] (%(funcName)s) %(message)s'
-    logging.basicConfig(format=log_format, datefmt='%Y-%m-%d %H:%M:%S')
+def create_logger(app_name=None, level='warning', path='logs/'):
+    # parm app_name (requried): application name
+    # parm level: loglevel; details see: https://docs.python.org/3/howto/logging.html 
+    # parm path: logs path; logs will be writes to this path with specified app_name.
+    # return: stream logger obj
+    logger = logging.getLogger(app_name)
+    logger.setLevel(level.upper())
+        
+    # Add stream handler, output all level
+    streamhd = logging.StreamHandler()
+    streamhd.setLevel(logging.DEBUG)
+    log_format = '[%(asctime)-15s] [%(levelname)08s] [%(filename)s] (%(funcName)s) (line: %(lineno)d): %(message)s'
+    formatter = logging.Formatter(fmt=log_format, datefmt='%Y-%m-%d %H:%M:%S')
+    streamhd.setFormatter(formatter)
+    logger.addHandler(streamhd)
+
+    # Add same format file handler, log warning and above level:
+    # Caller should specifiy app_name,
+    thisfile = os.path.basename(__file__).replace(".py","",1)
+    if app_name != thisfile and app_name:
+        if not os.path.isdir(path):
+            os.mkdir(path)
+        filehd = logging.FileHandler(filename=path+app_name+'.log', mode='w' ,encoding='utf-8')
+        filehd.setLevel(logging.WARNING)
+        filehd.setFormatter(formatter)
+        logger.addHandler(filehd)
     return logger
 
 def main():
-    LOGGER = create_logger(level='DEBUG')
+    # Sample Usage: (put in actual file)
+    logfile = os.path.basename(__file__).replace(".py","",1)
+    LOGGER = create_logger(app_name=logfile, level='DEBUG')
     LOGGER.debug("Running: %d", 0)
 
 if __name__ == "__main__":
